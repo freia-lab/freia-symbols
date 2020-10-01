9176378e5cf16c20d3f43aa3435c802f87ecfbce
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2020-10-01 17:19:10 +0200

    Added 2Kpumps+gas_bag display. Fixed AI simulation panel and script.

--- display-builder/CmdPanels/scripts/PVname2Macro.py
+++ display-builder/CmdPanels/scripts/PVname2Macro.py
@@ -2,6 +2,9 @@ from org.csstudio.display.builder.runtime.script import PVUtil
 
 channel = PVUtil.getString(pvs[0])
 
+# Remove the last part of the name
+channel = channel.replace(':sRdV','')
+
 # Adding the macros on the widget that will consume this script
 widget.getPropertyValue("macros").add("AI2Macro", channel)
 
