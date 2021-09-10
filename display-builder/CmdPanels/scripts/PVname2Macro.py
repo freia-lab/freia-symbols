bd9b666521d44d87fa59ccaf725a64eebe71d0e0
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2021-09-10 21:02:10 +0200

    Added simulation panel for DI.

--- display-builder/CmdPanels/scripts/PVname2Macro.py
+++ display-builder/CmdPanels/scripts/PVname2Macro.py
@@ -3,10 +3,26 @@ from org.csstudio.display.builder.runtime.script import PVUtil
 channel = PVUtil.getString(pvs[0])
 
 # Remove the last part of the name
-channel = channel.replace(':sRdV','')
-
+if ":sRdV" in channel:
+#    print("Analog")
+    channel = channel.replace(':sRdV','')
 # Adding the macros on the widget that will consume this script
-widget.getPropertyValue("macros").add("AI2Macro", channel)
+    widget.getPropertyValue("macros").add("AI2Macro", channel)
+    widget.setPropertyValue("file", "")
+    widget.setPropertyValue("file", "./Cstat-AI-sim.bob")
+elif ":sInp" in channel or ":sProcInp" in channel:
+#    print("Digital")
+    channel = channel.replace(':sInp','')
+    channel = channel.replace(':sProcInp','')
+    widget.getPropertyValue("macros").add("DI2Macro", channel)
+    widget.setPropertyValue("file", "")
+    widget.setPropertyValue("file", "./Cstat-DI-sim.bob")
+else:
+    widget.getPropertyValue("macros").add("AI2Macro", "")
+    widget.getPropertyValue("macros").add("DI2Macro", "")
+#    print("Unsupported PV name")
+# Adding the macros on the widget that will consume this script
+#widget.getPropertyValue("macros").add("AI2Macro", channel)
 
-widget.setPropertyValue("file", "")
-widget.setPropertyValue("file", "./Cstat-AI-sim.bob")
+#widget.setPropertyValue("file", "")
+#widget.setPropertyValue("file", "./Cstat-AI-sim.bob")
