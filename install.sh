39cda8faa66b264e6431c567dd10930939d364ae
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-06-24 12:25:01 +0200

    Updated Sauer On status logic. Updated installation path.

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -va --exclude '.git' --exclude 'install.sh' . /opt/epics/opi/boy/freia-symbols/
+rsync -va --exclude '.git' --exclude 'install.sh' . /opt/epics/opi/bob/freia-symbols/
